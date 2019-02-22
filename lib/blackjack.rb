3e19dd0379dcec2881a3563ab08f411c9f8d5a23
fortunate-partial-4781 <niko876@hotmail.com>
2019-02-22 02:04:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,10 +30,10 @@ end
 
 def hit?(card_total)
   prompt_user
-  get_user_input
-  if get_user_input == "s"
+  user_input = get_user_input
+  if user_input == "s"
     
-  elsif get_user_input == "h"
+  elsif user_input == "h"
     card_total += deal_card
   else
     invalid_command
